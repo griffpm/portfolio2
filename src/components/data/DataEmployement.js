d0e2edb503055a03e6d35742cf761028df1e9163
Menard Paul <menardlycee@gmail.com>
2023-08-22 12:08:25 +0200

    12

--- src/components/data/DataEmployement.js
+++ src/components/data/DataEmployement.js
@@ -6,5 +6,14 @@ export const DataEmployement = [
         date2:"28/07/2022",
         post:"developpeur ",
         id:"1"
+    },
+
+    {
+        entreprise:"Altameos Multimedia",
+        urlEntreprise:"https://www.linkedin.com/in/lucas-schnekenburger-b334a969/",
+        date1:"03/06/2023",
+        date2:"04/08/2023",
+        post:"developpeur ",
+        id:"2"
     }
 ]
