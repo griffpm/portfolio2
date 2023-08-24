d16999a06eaf800bf9d3292ee9ed1a656a44e932
paul <127496673+griffpm@users.noreply.github.com>
2023-08-24 09:27:55 +0200

    Update CardEmploi.js

--- src/components/cards/CardEmploi.js
+++ src/components/cards/CardEmploi.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CardEmploi({entreprise, urlEntreprise, date1, date2, post}) {
   return (
-    <div className='bg-[#c3c3c3] w-[300px] border-[2px] border-solid border-[var(--color-g)] mx-auto mt-4 p-2 text-[1rem] text-[var(--color-g)]'>
+    <div className='bg-[#c3c3c3] w-[300px] border-[2px] border-solid border-[var(--color-g)] mx-4 mt-4 p-2 text-[1rem] text-[var(--color-g)]'>
         <p className=" text-[1.5rem]">{entreprise}</p> 
         <a href={urlEntreprise}>Visiter le Site de l'entreprise</a> 
         <p>Du {date1} au {date2}</p> 
