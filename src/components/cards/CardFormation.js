e272b2c0be35ea97624c421ad33a06c156c31683
paul <127496673+griffpm@users.noreply.github.com>
2023-08-24 09:29:51 +0200

    Update CardFormation.js

--- src/components/cards/CardFormation.js
+++ src/components/cards/CardFormation.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CardFormation({nom, year, place}) {
   return (
-    <div className='bg-[#c3c3c3] w-[300px] border-[2px] border-solid border-[var(--color-g)] mx-auto my-4 p-2  '>
+    <div className='bg-[#c3c3c3] w-[300px] border-[2px] border-solid border-[var(--color-g)] mx-4 my-4 p-2  '>
         <div className='text-[var(--color-g)] text-[1rem]'>
             <p className='text-[1.5rem]'>{nom}</p>
             <p>{year}</p>
