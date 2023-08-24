be6c65de8671a56fbf4194613b5f321fe5d1fddc
paul <127496673+griffpm@users.noreply.github.com>
2023-08-24 10:08:15 +0200

    Update Contact.js

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
         <div id='contact_content'>
             <div id='cc-text'>
                 <h2>Contactez moi !</h2>
-                <p>Soit par <span>email </span> ou par <br/> mes <span>réseaux.</span></p>
+                <p>Soit par email: <span> menard@contact.com </span> ou par <br/> mes <span>réseaux.</span></p>
                 <img src="" alt="" />
             </div>
             <div id='cc-rs'>
@@ -15,11 +15,6 @@ export default function Contact() {
             <a href=""><FiTwitter size={32} className='my-4'/></a>
             <a target='_blank' href="https://www.linkedin.com/in/paul-menard-202251169/"><AiFillLinkedin size={32} className='my-4'/> </a>
             <a href=""><AiFillGithub size={32} className='my-4'/></a>
-            </div>
-            <div id="coordonees">
-                <h2> Mes Coordonnées </h2>
-                  <p>Mon Email: menard@gmail.com </p>
-                  <p>Mon téléphone: 0* ** ** ** ** </p>
             </div>
         </div>
     </div>
