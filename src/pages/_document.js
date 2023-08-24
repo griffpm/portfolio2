e6025225a7920bb57a029b8d964bfd65fff6ffaf
paul <127496673+griffpm@users.noreply.github.com>
2023-08-24 11:21:31 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="fr">
+    <title> Mon Portfolio </title>
       <Head />
       <body>
         <Main />
