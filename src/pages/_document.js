3c0d95524147ba4c86eb49e10d4fd9eda831a481
paul <127496673+griffpm@users.noreply.github.com>
2023-08-24 11:37:05 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,10 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="fr">
-    <title> Mon Portfolio </title>
+    <title> Mon Portfolio | Paul Menard Developpeur web </title>
     <meta
           name="description"
-          content="Je vous invite à découvrir un portfolio magnifique. Fait par un introvert pour les introvert. "
+          content="Je vous invite à découvrir un portfolio magnifique. Fait par un introvert pour les introvert. Vous pourrez, si vous visitez ce site, l'opportunité de voir un simple design, accompagner d'un leger élément de beauté. Je vous invite a constatez, avec vos yeux, mes compétences et mes projets. Je vous souhaite une bonne visite. "
           key="desc"
         />
       <Head />
