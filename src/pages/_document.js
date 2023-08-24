646f335c334ea4c820ba382eac0a0068686b03c0
paul <127496673+griffpm@users.noreply.github.com>
2023-08-24 11:25:18 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="fr">
     <title> Mon Portfolio </title>
+    <meta
+          name="description"
+          content="Je vous invite à découvrir un portfolio magnifique. Fait par un introvert pour les introvert. "
+          key="desc"
+        />
       <Head />
       <body>
         <Main />
