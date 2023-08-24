1435e57c8a86cba5568d0a728a8aec199925283e
paul <127496673+griffpm@users.noreply.github.com>
2023-08-24 13:42:11 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,10 +9,7 @@ export default function Document() {
           content="Je vous invite à découvrir un portfolio magnifique. Fait par un introvert pour les introvert.  Je vous invite a constatez, avec vos yeux, mes compétences et mes projets. "
           key="desc"
         />
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Tulpen+One&display=swap" rel="stylesheet">
-      <Head />
+     <Head />
       <body>
         <Main />
         <NextScript />
