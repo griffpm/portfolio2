45f253c1b2bd7a5c2bf756358a9f2604e815e823
paul <127496673+griffpm@users.noreply.github.com>
2023-08-24 09:55:48 +0200

    Update Contact.js

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -16,39 +16,9 @@ export default function Contact() {
             <a target='_blank' href="https://www.linkedin.com/in/paul-menard-202251169/"><AiFillLinkedin size={32} className='my-4'/> </a>
             <a href=""><AiFillGithub size={32} className='my-4'/></a>
             </div>
-            <div id='cc-form'>
-                {/* formulaire de contact */}
-                <form className='max-w-[420px]'>
-                    <div className='flex flex-wrap justify-between mb-[2rem]'>
-                        <div>
-                            <div className='my-2'>
-                                <label>Nom</label>
-                            </div>
-                            <div className='my-2'>
-                                <input type="text" className='h-[40px] text-white bg-[#35353c] border-[1px] border-solid border-[#a7a3a3]' />
-                            </div>
-                        </div>
-                        <div>
-                            <div className='my-2'>
-                                <label htmlFor="">Email</label>
-                            </div>
-                            <div className='my-2'>
-                                <input type="email" className='h-[40px] text-white bg-[#35353c] border-[1px] border-solid border-[#a7a3a3]' />
-                            </div>
-                        </div>
-                    </div>
-                    <div className='my-[3rem]'>
-                        <div className='my-2'>
-                            <label htmlFor="">Message</label>
-                        </div>
-                        <div>
-                            <textarea name="message" id="message" className='w-[100%] min-h-[100px] text-white bg-[#35353c] border-[1px] border-solid border-[#a7a3a3]'></textarea>
-                        </div>
-                    </div>
-                    <div className='flex justify-center'>
-                        <button className='text-white text-[1rem] border-green-400 border-solid border-[2px] py-2 px-2 '>Envoyez!</button>
-                    </div>
-                </form>
+            <div id="coordonees">
+                  <p>Mon Email: menard@gmail.com </p>
+                  <p>Mon téléphone: 0* ** ** ** ** </p>
             </div>
         </div>
     </div>
